43533e9b7077f8bfab39ef0a06d365f5df50443b
Michał Szleger <michal.szleger@gmail.com>
2024-12-10 22:28:36 +0100

    Refactor class Solver

--- app/solver.py
+++ app/solver.py
@@ -1,20 +1,69 @@
+from multipledispatch import dispatch
+import xml.etree.ElementTree as ET
+
 from app.maze import Maze
 from app.maze_solution import MazeSolution
 from app.utilities import call_command
 
 class Solver:
-    def __init__(self, name, command):
-        self.__name = name
-        self.__command = command
+    @dispatch(str, str, bool)
+    def __init__(self, name, command, checked):
+        self.name = name
+        self.command = command
+        self.checked = checked
 
-    def get_name(self):
-        return self.__name
+    @dispatch(ET.Element)
+    def __init__(self, xml_config_tree_root):
+        self.load(xml_config_tree_root)
 
-    def get_command(self):
-        return self.__command
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if type(name) != str:
+            raise ValueError("Name must be string")
+        if len(name) == 0:
+            raise ValueError("Name can't be empty string")
+        self._name = name
+
+    @property
+    def command(self):
+        return self._command
+
+    @command.setter
+    def command(self, command):
+        if type(command) != str:
+            raise ValueError("Command must be string")
+        if len(command) == 0:
+            raise ValueError("Command can't be empty string")
+        self._command = command
+
+    @property
+    def checked(self):
+        return self._checked
+
+    @checked.setter
+    def checked(self, checked):
+        if type(checked) != bool:
+            raise ValueError("Checked must be boolean")
+        self._checked = checked
+
+    def load(self, xml_config_tree_root):
+        self.name = xml_config_tree_root.find("name").text
+        self.command = xml_config_tree_root.find("command").text
+        self.checked = (xml_config_tree_root.find("checked").text == "True")
+
+    def save(self, xml_config_tree_root):
+        ET.SubElement(xml_config_tree_root, "name").text = self.name
+        ET.SubElement(xml_config_tree_root, "command").text = self.command
+        if self.checked:
+            ET.SubElement(xml_config_tree_root, "checked").text = "True"
+        else:
+            ET.SubElement(xml_config_tree_root, "checked").text = "False"
 
     def run(self, maze):
-        solution = MazeSolution(self.__name)
-        output = call_command(self.__command, maze.get_frame_for_solver())
-        solution.load_solution_from_solver(output)
+        output_frame = call_command(self.command, maze.get_frame_for_solver())
+        solution = MazeSolution(output_frame)
         return solution
--- tests/test_solver.py
+++ tests/test_solver.py
@@ -1,12 +1,64 @@
+import numpy as np
+import pytest
+import xml.etree.ElementTree as ET
+
 from app.maze import Maze
 from app.maze_solution import MazeSolution
 from app.solver import *
-import numpy as np
 
-def test_run_solver():
+def test_default_constructor():
+    s = Solver("Solver Name", "Solver Command", True)
+    assert s.name == "Solver Name"
+    assert s.command == "Solver Command"
+    assert s.checked == True
+
+def test_xml_loading_constructor():
+    tree = ET.parse("tests/data/xml/solver.xml")
+    root = tree.getroot()
+    s = Solver(root)
+    assert s.name == "Solver Name"
+    assert s.command == "Solver Command"
+    assert s.checked == True
+
+def test_name():
+    s = Solver("Solver Name", "Solver Command", True)
+    with pytest.raises(ValueError, match="Name must be string"):
+        s.name = 5
+    with pytest.raises(ValueError, match="Name can't be empty string"):
+        s.name = ""
+
+def test_command():
+    s = Solver("Solver Name", "Solver Command", True)
+    with pytest.raises(ValueError, match="Command must be string"):
+        s.command = 5
+    with pytest.raises(ValueError, match="Command can't be empty string"):
+        s.command = ""
+
+def test_checked():
+    s = Solver("Solver Name", "Solver Command", True)
+    with pytest.raises(ValueError, match="Checked must be boolean"):
+        s.checked = 5
+
+def test_load():
+    s = Solver("Default", "Default", False)
+    tree = ET.parse("tests/data/xml/solver.xml")
+    root = tree.getroot()
+    s.load(root)
+    assert s.name == "Solver Name"
+    assert s.command == "Solver Command"
+    assert s.checked == True
+
+def test_save():
+    s = Solver("Solver Name", "Solver Command", True)
+    root = ET.Element("config")
+    s.save(root)
+    assert root.find("name").text == "Solver Name"
+    assert root.find("command").text == "Solver Command"
+    assert root.find("checked").text == "True"
+
+def test_run():
     m = Maze(np.array([2, 2]), 0)
-    s = Solver("DummySolver", "python tests/data/solver/dummy_solver.py")
-    solution = s.run(m)
-    assert solution.get_solver_name() == s.get_name()
-    assert solution.get_solving_time() == 1000
-    assert (solution.get_path() == np.array([[1, 2], [3, 4]])).all()
+    s = Solver("DummySolver", "python tests/data/solver/dummy_solver.py", False)
+    maze_solution = s.run(m)
+    assert maze_solution.solving_time_us == 1000
+    assert (maze_solution.path == np.array([[1, 2], [3, 4]])).all()
