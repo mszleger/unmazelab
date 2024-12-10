ebc9c8f0461ef9d2fe0814849869e4cddf7d7ca1
Michał Szleger <michal.szleger@gmail.com>
2024-12-11 00:17:59 +0100

    Refactor class GridWanderer

--- app/grid_wanderer.py
+++ app/grid_wanderer.py
@@ -2,31 +2,53 @@ import numpy as np
 
 class GridWanderer:
     def __init__(self, size):
-        self.__size = size
-        self.__visited_tiles = np.zeros((size[0], size[1]), dtype=bool)
+        self.size = size
 
-    def set_current_pos(self, pos):
-        self.__current_pos = pos
-        self.__visited_tiles[pos[0]][pos[1]] = True
+    @property
+    def size(self):
+        return self._size
 
-    def get_current_pos(self):
-        return self.__current_pos
+    @size.setter
+    def size(self, size):
+        if type(size) != np.ndarray:
+            raise ValueError("Size must be numpy.ndarray")
+        if size.size != 2 or size.shape[0] != 2:
+            raise ValueError("Size must be one dimensional array with two values")
+        if (size < 2).any():
+            raise ValueError("Both values of size must be equal or greater than 2")
+        self._size = size
+        self._visited_tiles = np.zeros((size[0], size[1]), dtype=bool)
+
+    @property
+    def current_pos(self):
+        return self._current_pos
+
+    @current_pos.setter
+    def current_pos(self, current_pos):
+        if type(current_pos) != np.ndarray:
+            raise ValueError("Current position must be numpy.ndarray")
+        if current_pos.size != 2 or current_pos.shape[0] != 2:
+            raise ValueError("Current position must be one dimensional array with two values")
+        if (current_pos < 0).any() or current_pos[0] >= self.size[0] or current_pos[1] >= self.size[1]:
+            raise ValueError("Current position is outside of the grid")
+        self._current_pos = current_pos
+        self._visited_tiles[current_pos[0]][current_pos[1]] = True
 
     def get_all_neighbours(self):
         vects_to_neighbours = np.array([[-1,  0],
                                         [ 1,  0],
                                         [ 0, -1],
                                         [ 0,  1]])
-        neighbours = self.__current_pos + vects_to_neighbours
+        neighbours = self.current_pos + vects_to_neighbours
         neighbours = neighbours[(neighbours[:, 0] >= 0)
                               & (neighbours[:, 1] >= 0)
-                              & (neighbours[:, 0] <  self.__size[0])
-                              & (neighbours[:, 1] <  self.__size[1])]
+                              & (neighbours[:, 0] <  self.size[0])
+                              & (neighbours[:, 1] <  self.size[1])]
         return np.array(neighbours)
 
     def get_unvisited_neighbours(self):
         unvisited_neighbours = []
         for pos in self.get_all_neighbours():
-            if not self.__visited_tiles[pos[0]][pos[1]]:
+            if not self._visited_tiles[pos[0]][pos[1]]:
                 unvisited_neighbours.append(pos)
         return np.array(unvisited_neighbours)
--- tests/test_grid_wanderer.py
+++ tests/test_grid_wanderer.py
@@ -1,31 +1,44 @@
-from app.grid_wanderer import *
 import numpy as np
+import pytest
 
-def test_pos_set_get():
+from app.grid_wanderer import *
+
+def test_size():
     gw = GridWanderer(np.array([5, 5]))
-    gw.set_current_pos(np.array([3, 2]))
-    assert (gw.get_current_pos() == np.array([3, 2])).all()
-    gw.set_current_pos(np.array([4, 1]))
-    assert (gw.get_current_pos() == np.array([4, 1])).all()
+    with pytest.raises(ValueError, match="Size must be numpy.ndarray"):
+        gw.size = 5
+    with pytest.raises(ValueError, match="Size must be one dimensional array with two values"):
+        gw.size = np.array([1, 2, 3])
+    with pytest.raises(ValueError, match="Both values of size must be equal or greater than 2"):
+        gw.size = np.array([1, 2])
+
+def test_current_pos():
+    gw = GridWanderer(np.array([5, 5]))
+    with pytest.raises(ValueError, match="Current position must be numpy.ndarray"):
+        gw.current_pos = 5
+    with pytest.raises(ValueError, match="Current position must be one dimensional array with two values"):
+        gw.current_pos = np.array([1, 2, 3])
+    with pytest.raises(ValueError, match="Current position is outside of the grid"):
+        gw.current_pos = np.array([-1, 5])
 
 def test_get_all_neighbours_middle():
     gw = GridWanderer(np.array([5, 5]))
-    gw.set_current_pos(np.array([2, 2]))
+    gw.current_pos = np.array([2, 2])
     assert (gw.get_all_neighbours() == np.array([[1, 2], [3, 2],[2, 1], [2, 3]])).all()
 
 def test_get_all_neighbours_top_left_corner():
     gw = GridWanderer(np.array([5, 5]))
-    gw.set_current_pos(np.array([0, 0]))
+    gw.current_pos = np.array([0, 0])
     assert (gw.get_all_neighbours() == np.array([[1, 0], [0, 1]])).all()
 
 def test_get_all_neighbours_bottom_right_corner():
     gw = GridWanderer(np.array([5, 5]))
-    gw.set_current_pos(np.array([4, 4]))
+    gw.current_pos = np.array([4, 4])
     assert (gw.get_all_neighbours() == np.array([[3, 4], [4, 3]])).all()
 
 def test_get_unvisited_neighbours():
     gw = GridWanderer(np.array([5, 5]))
-    gw.set_current_pos(np.array([1, 2]))
-    gw.set_current_pos(np.array([2, 1]))
-    gw.set_current_pos(np.array([2, 2]))
+    gw.current_pos = np.array([1, 2])
+    gw.current_pos = np.array([2, 1])
+    gw.current_pos = np.array([2, 2])
     assert (gw.get_unvisited_neighbours() == np.array([[3, 2], [2, 3]])).all()
