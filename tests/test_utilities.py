3ba8a83a9986cf395895a7ac9f9f323114cd5d88
Michał Szleger <michal.szleger@gmail.com>
2024-12-14 23:46:43 +0100

    Refactor call_command utility tests

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -1,5 +1,7 @@
 from app.utilities import *
 
-def test_call_command():
+def test_call_command_with_options():
     assert call_command('ls tests/data/call_command', '') == 'file_1.txt\nfile_2.txt'
+
+def test_call_command_stdio_stdout_communication():
     assert call_command('cat', 'abcd') == 'abcd'
