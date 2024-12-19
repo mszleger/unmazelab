156bdd584c75718609b68b1c602b228229d1846c
Michał Szleger <michal.szleger@gmail.com>
2024-12-19 20:28:43 +0100

    Add documentation of module utilities

--- app/utilities.py
+++ app/utilities.py
@@ -1,6 +1,15 @@
 import subprocess
 
 def call_command(command, stdin):
+    """
+    Calls command in command line.
+
+    :param stdin: String with standard input to send to runned command.
+    :type stdin: str
+
+    :return: Standard output returned by runned command.
+    :rtype: str
+    """
     command = command.split()
     proc = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
     proc.stdin.write(stdin.encode("utf8"))
