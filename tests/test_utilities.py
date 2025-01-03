483600f55cf75d72b9806f55a28c4516888db69f
Michał Szleger <michal.szleger@gmail.com>
2025-01-03 15:11:38 +0100

    Improve test's docstring

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -5,7 +5,7 @@ from app import utilities
 def test_call_command():
     '''Tests function call_command.
 
-    Runs program returning everything sent to it's standard input
+    Runs program 'echo' returning everything sent to it's standard input
     on it's standard output.
     '''
     cmd = 'python tests/scripts/echo.py'
